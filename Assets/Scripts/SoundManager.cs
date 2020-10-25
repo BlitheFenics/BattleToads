0cde2f78c4544f2fe795634f89b21e01c1c840ac
Phoenix <phoenixmakins@hotmail.com>
2020-10-24 20:44:53 -0400

    BGM/SFX
    background music and button sound effects added

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource button;
+
+    public void PlayButton()
+    {
+        button.Play();
+    }
+}
